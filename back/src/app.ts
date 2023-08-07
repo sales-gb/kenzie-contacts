f18334b45bca0987b5d575d52274a7b41b1c5508
Gabriel Sales <gabrielsales081@gmail.com>
2023-08-07 02:11:51 -0300

    feat: add new cors to vercel link

--- back/src/app.ts
+++ back/src/app.ts
@@ -8,8 +8,22 @@ import cors from "cors";
 const app: Application = express();
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:3001" }));
+const allowedOrigins = [
+  "http://localhost:3001",
+  "https://kenzie-contacts-mu.vercel.app",
+];
 
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 app.use("/clients", clientsRoutes);
 app.use("/login", loginRoutes);
 app.use("/contacts", contactRoutes);
