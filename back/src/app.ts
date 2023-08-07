d3b8923710a80499042cda3ddf191cc4bbf7e9e2
Gabriel Sales <gabrielsales081@gmail.com>
2023-08-07 12:07:47 -0300

    chore: update cors

--- back/src/app.ts
+++ back/src/app.ts
@@ -8,22 +8,8 @@ import cors from "cors";
 const app: Application = express();
 app.use(express.json());
 
-const allowedOrigins = [
-  "http://localhost:3001",
-  "https://kenzie-contacts-mu.vercel.app",
-];
+app.use(cors());
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
 app.use("/clients", clientsRoutes);
 app.use("/login", loginRoutes);
 app.use("/contacts", contactRoutes);
